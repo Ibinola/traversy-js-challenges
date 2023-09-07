6a66932926409659fcb22eb19f9b5f043d4a8085
Michael Ibinola <ibinolamichael1@gmail.com>
2023-09-08 00:20:06 +0100

    basic challenges: 3 done

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,19 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+    if(operator == "+"){
+        return num1 + num2;
+    }
+    if(operator == "-"){
+        return num1 - num2;
+    }
+    if(operator == "*"){
+        return num1 * num2;
+    }
+    if(operator == "/"){
+        return num1 / num2;
+    }
+    if(operator !== "+" || operator !== "-" || operator !== "*" || operator !== "/"){
+        return "Invalid operator"
+    }
+}
 
 module.exports = calculator;
