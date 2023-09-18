88d1fdb3f7e146fcc9e408f68a935e5278f79325
Michael Ibinola <ibinolamichael1@gmail.com>
2023-09-18 13:22:18 +0100

    07/07

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,29 @@
-function reverseString() {}
+function reverseString(string) {
+    // hello world-> olleh
+    //  ij 
+    let reverseString = string.split('')
+    let start = 0;
+    let end = string.length - 1;
+
+
+    while (start < end) {
+        [reverseString[start], reverseString[end]] = [reverseString[end], reverseString[start]]
+        // let temp = str[start];
+        // str[start] = str[end];
+        // str[end] = temp;
+        start++;
+        end--;
+    }
+
+    return reverseString.join('');
+
+    // let str = ''
+    // for (let i = string.length - 1; i >= 0; i--) {
+    //     str += string[i];
+    // }
+
+    // return str;
+
+}
 
 module.exports = reverseString;
