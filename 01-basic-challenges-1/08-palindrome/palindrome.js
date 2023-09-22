734e9a1bc90e814360035fd8c1407ef661f87314
Michael Ibinola <ibinolamichael1@gmail.com>
2023-09-22 13:02:30 +0100

    palindrome complete

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,20 @@
-function isPalindrome() {}
+function isPalindrome(string) {
+    //madam
+    // let i = 0;
+    // let j = string.length - 1;
+    // let cleanedString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase().split('').join('')
+
+    // while (i < j) {
+    //     if (cleanedString[i] !== cleanedString[j]) {
+    //         return false;
+    //     }
+    //     i++;
+    //     j--;
+    // }
+    let cleanedString = string.toLowerCase().replace(/[^a-z0-9]/g, '')
+    let reversedStr = cleanedString.split('').reverse().join('')
+
+    return cleanedString === reversedStr;
+}
 
 module.exports = isPalindrome;
